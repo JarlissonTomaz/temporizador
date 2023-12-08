5ec0d776accdbd8c63f1884d8235dfe6e1e2d6e0
JarlissonTomaz <tomazj995@gmail.com>
2023-12-07 21:24:42 -0300

    teste

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha lista de brinquedos',
+            style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: Column(children: [
+        const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
+              hintText: 'Insira novo brinquedo',
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                alignment: Alignment.center,
+                width: 240,
+                height: 30,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 0.5,
+                    )),
+                child: const Text('Quantidade de Brinquedos : 0'),
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              width: 140,
+              height: 30,
+              decoration: const BoxDecoration(
+                  color: Color.fromRGBO(12, 125, 161, 1),
+                  borderRadius: BorderRadius.all(Radius.circular(25))),
+              child: const Text('Adicionar',
+                  style: TextStyle(color: Colors.white)),
+            )
+          ],
+        )
+      ]),
+    );
   }
 }
